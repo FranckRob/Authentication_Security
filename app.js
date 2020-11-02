183865851af48b970b2d75974cd668e9ac6edd98
Francois Robichaud <francois.robichaud@thinkrf.com>
2020-11-02 15:18:29 -0500

    Adding Hashing and Salting Password

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt =require("mongoose-encryption");
+const bcrypt = require("bcrypt"); // Level 4 : Salting
+const saltRounds = 10; // Level 4 : Nb of rounds
+//const md5 = require("md5");  // Level 3 : Hashing
+//const encrypt =require("mongoose-encryption"); //Level 1 : Database content encryption
 
 app = express();
 
@@ -12,14 +15,14 @@ app.use(express.static("public"));
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({entended: true},{ useUnifiedTopology: true }));
 
-mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser : true});
+mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser: true});
 
 const userSchema = new mongoose.Schema({
   email : String,
   password : String
 });
 
-userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields: ["password"]});
+//userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields: ["password"]}); //Level 1 : Database content encryption
 
 const User = new mongoose.model("User", userSchema);
 
@@ -36,35 +39,42 @@ app.get("/register",function(req,res){
 });
 
 app.post("/register", function(req,res){
-  const newUser = new User({
-    email : req.body.username,
-    password : req.body.password
-  });
 
-  newUser.save(function(err){
-    if(!err){
-      res.render("secrets");
-    }else {
-      console.log(err);
-    }
+  bcrypt.hash(req.body.password, saltRounds, function(err,hash){ // Level 4 : Salting
+    const newUser = new User({
+      email : req.body.username,
+      //password : md5(req.body.password) // Level 3 : md5() ---- Hashing the password entered for registration
+      password : hash
+    });
+    newUser.save(function(err){
+      if(!err){
+        res.render("secrets");
+      }else {
+        console.log(err);
+      }
+    });
   });
 });
 
 app.post("/login",function(req,res){
   const username = req.body.username;
+  //const password = md5(req.body.password); // Level 3 :  md5() ---- Hashing the password entered for login
   const password = req.body.password;
+
   User.findOne({email: username},function(err,foundUser){
     if(err){
       console.log(err);
     }else {
       if(foundUser){
-        if(foundUser.password===password){
-          res.render("secrets");
+        //if(foundUser.password===password){ // Replace this line for bcrypt.compare license
+        bcrypt.compare(password, foundUser.password, function(err,result){
+          if(result===true) {
+            res.render("secrets");
+          }
+        });
         }
       }
-    }
   });
-
 });
 
 app.listen(3000,function(){
